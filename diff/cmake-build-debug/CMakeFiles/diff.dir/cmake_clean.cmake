d5b06b4e848c46a5d2ade0a207257a955ad0b4cb
Egor <giksengik@gmail.com>
2020-12-13 14:55:56 +0300

    add diff

---
+++ diff/cmake-build-debug/CMakeFiles/diff.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/diff.dir/main.cpp.obj"
+  "diff.exe"
+  "diff.exe.manifest"
+  "diff.pdb"
+  "libdiff.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/diff.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ diff/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int N;//количество элементов в массиве
+    cin>>N;
+    int delay;
+    cin>>delay;
+    for(int i=0;i<delay;i++){
+        rand();
+    }
+    int arr [N];
+    for(int i=0;i<N;i++){
+        if(i==0){
+            arr[i]=(int)(rand()/10);
+        }else {
+           int k=(int)(rand()/10);
+           while(k<arr[i-1]){
+               k=(int)(rand()/10);
+           }
+           arr[i]=k;
+        }
+    }
+    for(int i=0;i<N;i++) {
+        cout<<arr[i]<<endl;
+    }
+    int count=0;
+    for(int i=0;i<N;i++){
+        if(i==0) count++;
+        else {
+            if(arr[i]!=arr[i-1]) count++;
+        }
+    }
+    cout<< count<<endl;
+}
